ac3fdad220e48df0825ccbed408fd972b2a8642d
fulei.yang <fulei.yfl@alibaba-inc.com>
2018-04-13 10:11:08 +0800

    http test

--- src/main/httputil/httpoperation.go
+++ src/main/httputil/httpoperation.go
@@ -9,14 +9,13 @@ import (
 	"strconv"
 	"time"
 	"io/ioutil"
+	"gopractice/src/main/stringutil"
 )
-
-
 
 func HandleRequest() {
 	rounter := mux.NewRouter()
 	rounter.HandleFunc("/info/{action}", HandleGet).Methods("GET")
-	rounter.Handle("/handler/{action}",&Example{})
+	rounter.Handle("/handler/{action}", &Example{})
 	http.ListenAndServe(":8080", rounter)
 }
 
@@ -26,7 +25,7 @@ func HandleGet(w http.ResponseWriter, req *http.Request) {
 	go ServiceInfo()
 	time.Sleep(time.Second)
 	if action == "test" {
-		res, err:=ioutil.ReadFile("info.json")
+		res, err := ioutil.ReadFile("info.json")
 		if nil != err {
 			panic(err)
 		}
@@ -53,4 +52,37 @@ func GoID() int {
 		panic(fmt.Sprintf("cannot get goroutine id: %v", err))
 	}
 	return id
+}
+
+//发送GET请求，返回Body中的数据
+func SendGetRequest(url string) string {
+	resp, err := http.Get(url)
+	if nil != err {
+		panic(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	return stringutil.Byte2Str(body)
+}
+//发送POST请求，返回Body中的数据
+func SendPostRequest (url string) string{
+	resp,err:=http.Post(url,"application/x-www-form-urlencoded",strings.NewReader(""))
+	if err!=nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	return stringutil.Byte2Str(body)
+}
+
+func SendRequest(){
+	client:=http.DefaultClient
+	request :=&http.Request{}
+	client.Do(request)
+}
+
+func buildRequest(){
+
 }
--- src/main/httputil/httpoperation_test.go
+++ src/main/httputil/httpoperation_test.go
@@ -5,3 +5,8 @@ import "testing"
 func TestHandleRequest(t *testing.T) {
 	HandleRequest()
 }
+
+func TestSendGetRequest(t *testing.T) {
+	res:=SendGetRequest("http://localhost:8080/info/test")
+	println(res)
+}
