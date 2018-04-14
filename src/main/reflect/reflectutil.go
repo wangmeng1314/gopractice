c3745f288919cbf04fde2f279882b1db2dba81ac
fulei.yang <fulei.yfl@alibaba-inc.com>
2018-04-14 18:35:36 +0800

    反射

--- src/main/reflect/reflectutil.go
+++ src/main/reflect/reflectutil.go
@@ -9,19 +9,25 @@ type Info struct {
 	Name string
 }
 
-func (i *Info) InvokeMethod() {
+func (*Info) InvokeMethod() {
 	fmt.Println("test")
 }
 
 func GetType() {
-	info := Info{"dream"}
+	info := &Info{"dream"}
 
 	infotype := reflect.TypeOf(info)
-	println(infotype.Name())
+	fmt.Println("string",infotype.Method(0))
+
+	value:=reflect.ValueOf(info)
+	fmt.Println("string",value.Method(0))
+
 	for i:=0;i<infotype.NumMethod() ;i++  {
 		println(infotype.Method(i).Name)
 	}
+
 	methodRes,err:=infotype.MethodByName("InvokeMethod")
 	println(err)
 	fmt.Println(methodRes)
+
 }
