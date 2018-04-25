4c03244fdb9d6eb679b8817659f95de7715c4cbd
fulei.yang <fulei.yfl@alibaba-inc.com>
2018-04-25 09:55:20 +0800

    文件读取

--- src/main/fileutil/file.go
+++ src/main/fileutil/file.go
@@ -1,11 +1,26 @@
 package fileutil
 
-import "fmt"
+import (
+	"io/ioutil"
+	"gopractice/src/main/stringutil"
+)
 
 type Reader interface {
-	call()
+	readFile() string
 }
 
-func call() {
-	fmt.Print("")
+type FileProcessor struct {
+	path string
+}
+
+func (fprocessor *FileProcessor) readFile() string {
+	resByte, err := ioutil.ReadFile(fprocessor.path)
+
+	if err != nil {
+		panic(err)
+	}
+
+	res := stringutil.Byte2Str(resByte)
+	return res
+
 }
