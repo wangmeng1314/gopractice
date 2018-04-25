84f7495f542a6c5e88e0998357ef9a39fa7d8a32
fulei.yang <fulei.yfl@alibaba-inc.com>
2018-04-25 13:18:07 +0800

    写入文件

--- src/main/fileutil/file.go
+++ src/main/fileutil/file.go
@@ -3,16 +3,21 @@ package fileutil
 import (
 	"io/ioutil"
 	"gopractice/src/main/stringutil"
+	"os"
 )
 
 type Reader interface {
 	readFile() string
 }
 
+type Writer interface {
+	writeFile(path string,data string)
+}
+
 type FileProcessor struct {
 	path string
 }
-
+//读取文件内容
 func (fprocessor *FileProcessor) readFile() string {
 	resByte, err := ioutil.ReadFile(fprocessor.path)
 
@@ -21,6 +26,10 @@ func (fprocessor *FileProcessor) readFile() string {
 	}
 
 	res := stringutil.Byte2Str(resByte)
-	return res
 
+	return res
+}
+//写入文件
+func (fprocessor *FileProcessor) writeFile(path string,data string) {
+	ioutil.WriteFile(path,stringutil.Str2Byte(data),os.ModeDevice)
 }
